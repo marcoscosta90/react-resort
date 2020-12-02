522d7c19a064ff90bc5a6e56739a912c56b351eb
Marcos Costa <50223741+marcoscosta90@users.noreply.github.com>
2020-12-02 15:35:57 -0300

    Contentful

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-    space: q3t6f1xsojs0,
-    accessToken: _1PU3BznmQbLbPQ2mcwFWlLHtJekOyNU8eQliyrgimk
-});
+    space: 'q3t6f1xsojs0',
+    accessToken: '_1PU3BznmQbLbPQ2mcwFWlLHtJekOyNU8eQliyrgimk'
+})
