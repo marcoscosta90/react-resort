9b0e92491f02809f02cd1c4dadfce39112b0fa0e
Marcos Costa <50223741+marcoscosta90@users.noreply.github.com>
2020-12-02 15:34:43 -0300

    Commit

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful';
 
 export default createClient({
-    space: process.env.REACT_APP_API_SPACE,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space: q3t6f1xsojs0,
+    accessToken: _1PU3BznmQbLbPQ2mcwFWlLHtJekOyNU8eQliyrgimk
 });
